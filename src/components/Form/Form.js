5400f30e8cad88bdafb69631a98d78ad19026414
Kayla Wood <kaylaewood@gmail.com>
2019-12-03 14:21:10 -0700

    Add styling to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './Form.css';
 
 class Form extends Component {
   render() {
-    return <p>form paragraph</p>
+    return (
+      <form>
+        <p className='form-text'>WHAT IS YOUR FIRST NAME?</p>
+        <input type='textbox' value=''/>
+        <p className='form-text'>WHAT IS YOUR FAVORITE STAR WARS QUOTE?</p>
+        <input type='textbox' value=''/>
+        <p className='form-text'>HOW WELL DO YOU KNOW STAR WARS?</p>
+        <select>
+          <option value='jedi-master'>JEDI MASTER</option>
+          <option value='jedi-knight'>JEDI KNIGHT</option>
+          <option value='padawon'>PADAWON</option>
+        </select>
+      </form>
+    )
   }
-}
-
+};
 export default Form;
