b1396448e142a609f87593b7613594bea0b9540d
Kayla Wood <kaylaewood@gmail.com>
2019-12-03 13:43:22 -0700

    Create skeleton for landing page components

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Form from '../Form/Form'
+
+function App() {
+  return (
+    <main className="App">
+      <Header />
+      <Form />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return <p>form paragraph</p>
+  }
+}
+
+export default Form;
