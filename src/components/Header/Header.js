d0a1cc8bc8d206d94881fc588aba1709fe7d0379
Kayla Wood <kaylaewood@gmail.com>
2019-12-03 14:04:19 -0700

    Add styling for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import deathStar from '../../images/death-star.png';
+import './Header.css';
 
 function Header() {
   return (
-    <h1>header text</h1>
+    <header>
+      <div className='deathStarContainer'>
+        <img src={deathStar}/>
+      </div>
+      <h1>WELCOME, YOUNG JEDI</h1>
+    </header>
   );
 }
 
