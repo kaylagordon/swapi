d92bab79e5531bfab98d9427c963c10b4288ea27
Kayla Wood <kaylaewood@gmail.com>
2019-12-03 15:38:54 -0700

    Add state to Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      favoriteQuote: '',
+      ranking: ''
+    }
+  }
+
+  updateUserInfo = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <form>
         <p className='form-text'>WHAT IS YOUR FIRST NAME?</p>
-        <input type='textbox' value=''/>
+        <input
+          type='text'
+          name='name'
+          value={this.state.name}
+          onChange={this.updateUserInfo}
+        />
         <p className='form-text'>WHAT IS YOUR FAVORITE STAR WARS QUOTE?</p>
-        <input type='textbox' value=''/>
+        <input
+          type='text'
+          name='favoriteQuote'
+          value={this.state.favoriteQuote}
+          onChange={this.updateUserInfo}
+        />
         <p className='form-text'>HOW WELL DO YOU KNOW STAR WARS?</p>
         <select>
-          <option value='jedi-master'>JEDI MASTER</option>
-          <option value='jedi-knight'>JEDI KNIGHT</option>
-          <option value='padawon'>PADAWON</option>
+          // <option value='jedi-master'>JEDI MASTER</option>
+          // <option value='jedi-knight'>JEDI KNIGHT</option>
+          // <option value='padawon'>PADAWON</option>
         </select>
         <button type='button'>LET'S GO</button>
       </form>
