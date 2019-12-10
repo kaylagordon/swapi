79713ebaf43de741f217bca2850eabbec3dad192
Kayla Wood <kaylaewood@gmail.com>
2019-12-09 21:21:35 -0700

    Add CharacterCard rendering to CharacterContainer snapshot test

--- src/components/CharacterContainer/CharacterContainer.test.js
+++ src/components/CharacterContainer/CharacterContainer.test.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CharacterContainer from './CharacterContainer';
+import CharacterCard from '../CharacterCard/CharacterCard';
+
 import { shallow } from 'enzyme';
 
 describe('CharacterContainer', () => {
   let characterURLs = ['url1', 'url2']
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<CharacterContainer
+  let wrapper;
+  let character1 = {
+    films: ['URL', 'anotherURL'],
+    homeworld: 'URL',
+    species: 'URL',
+    name: 'Billy'
+  }
+  let character2 = {
+    films: ['URL', 'anotherURL'],
+    homeworld: 'URL',
+    species: 'URL',
+    name: 'Bob'
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<CharacterContainer
       characterURLs={characterURLs}
     />)
-    expect(wrapper).toMatchSnapshot();
+
+    wrapper.setState({
+      isLoading: false,
+      characters: [character1, character2]
     })
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 })
