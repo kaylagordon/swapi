e2878ed17a64b4f2e910ed32d0311e629ed095b0
Kayla Wood <kaylaewood@gmail.com>
2019-12-10 14:44:19 -0700

    Updates

---
+++ precache-manifest.560ca6a52069874fb4c3890b3209549d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188f554d53914d82adfc2813c2227bd9",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "df1aea759b0602bc9b28",
+    "url": "/swapi/static/css/main.0bb0a382.chunk.css"
+  },
+  {
+    "revision": "018cdc012c6bd774c9be",
+    "url": "/swapi/static/js/2.f00e339a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/swapi/static/js/2.f00e339a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df1aea759b0602bc9b28",
+    "url": "/swapi/static/js/main.5f627ae2.chunk.js"
+  },
+  {
+    "revision": "b71ff6c98d137b14ac18",
+    "url": "/swapi/static/js/runtime-main.5f4ae7f1.js"
+  },
+  {
+    "revision": "3f5a0741b9d26e3562ce2a58854058a5",
+    "url": "/swapi/static/media/background.3f5a0741.jpg"
+  },
+  {
+    "revision": "225493aabaa4384ba94b8e7479a9e81b",
+    "url": "/swapi/static/media/death-star.225493aa.png"
+  }
+]);
