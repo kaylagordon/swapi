b6753548224c034f8a79c0b1b0864cefb61f5fd0
Kayla Wood <kaylaewood@gmail.com>
2019-12-09 21:48:32 -0700

    Add testing for methods in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -6,7 +6,7 @@ import { shallow } from 'enzyme';
 describe('App', () => {
   let wrapper;
 
-  beforeEach(() {
+  beforeEach(() => {
     wrapper = shallow(<App />)
 
   })
@@ -16,14 +16,30 @@ describe('App', () => {
   })
 
   it('should update state with form information', () => {
+    wrapper.instance().updateStateFromForm('kayla', 'pants are cool', 'padawon');
 
+    expect(wrapper.state()).toEqual({
+      loggedIn: true,
+      name: 'kayla',
+      favoriteQuote: 'pants are cool',
+      ranking: 'padawon',
+      characterURLs: [],
+      movies: []
+    })
   })
 
-  it('should update state with character URLs', () => {
+  it('should update state with the first 10 character URLs', () => {
+    let characters = ['URL1', 'URL2', 'URL3', 'URL4', 'URL5', 'URL6', 'URL7', 'URL8', 'URL9', 'URL10', 'URL11']
 
+    wrapper.instance().updateStateWithCharacters(characters);
+
+    expect(wrapper.state('characterURLs')).toEqual(['URL1', 'URL2', 'URL3', 'URL4', 'URL5', 'URL6', 'URL7', 'URL8', 'URL9', 'URL10']);
   })
 
   it('should update state when user logs out', () => {
-    
+    wrapper.setState({ loggedIn: true })
+    wrapper.instance().logOut();
+
+    expect(wrapper.state('loggedIn')).toEqual(false)
   })
 })
